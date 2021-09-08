dda6d9c0a01c752ef448da4ece52ed65175bb52c
venkat9898 <venky15.05s@gmail.com>
2021-09-08 18:10:12 +0530

    use

---
+++ src/org/sample/First.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class First {
+	public void fire() {
+		System.out.println("fire");
+		}
+		public void ice() {
+		System.out.println("ice");
+		}
+		public void water() {
+		System.out.println("waters");
+		}
+}
